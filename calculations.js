24e53e1fdbfc5a494d8906d604f9af36c6b96091
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2024-04-24 01:41:45 +0200

    [HW_2_2_4] added increment and decrement functions

--- calculations.js
+++ calculations.js
@@ -1,20 +1,30 @@
 function increaseBalance(x, y) {
-  return x += y;
+  return (x += y);
 }
 
 function decreaseBalance(x, y) {
-  return x -= y;
+  return (x -= y);
 }
 
 function divideBalanceByAccounts(x, y) {
-  return x /= y;
+  return (x /= y);
 }
 
 function getRestAfterDivision(x, y) {
-  return x %= y;
+  return (x %= y);
+}
+
+function incrementNumber(x) {
+  return ++x;
+}
+
+function decrementNumber(x) {
+  return --x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(incrementNumber(29));
+console.log(decrementNumber(29));
